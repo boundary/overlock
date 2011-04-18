13ceb589d40502cbc51cc61e4c6a0164e0d14a0f
cliff moon <cliff@leaninto.it>
2011-04-18 15:26:03 -0700

    sbt publish

--- project/build/OverlockProject.scala
+++ project/build/OverlockProject.scala
@@ -4,6 +4,10 @@ import sbt.StringUtilities._
 class OverlockProject(info : ProjectInfo) extends DefaultProject(info) {
   val codaRepo = "Coda Hale's Biznazz" at "http://repo.codahale.com/"
   val boundary = "Boundary Public Repo" at "http://maven.boundary.com/artifactory/repo"
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Boundary Public Repo (Publish)"
+  
+  Credentials(Path.userHome / ".ivy2" / ".credentials-external", log)
   
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test"
   val metrics = "com.yammer" %% "metrics" % "2.0.0-BETA10"
