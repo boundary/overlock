169b065ec7b44a4b81feb25251fbf7211f4361be
cliff moon <cliff@leaninto.it>
2011-04-18 09:17:29 -0700

    use the boundary public repo to pull in a modified high scale lib

--- project/build/ThreadpoolProject.scala
+++ project/build/ThreadpoolProject.scala
@@ -3,8 +3,9 @@ import sbt.StringUtilities._
 
 class ThreadpoolProject(info : ProjectInfo) extends DefaultProject(info) {
   val codaRepo = "Coda Hale's Biznazz" at "http://repo.codahale.com/"
+  val boundary = "Boundary Public Repo" at "http://maven.boundary.com/artifactory/repo"
   
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test"
   val metrics = "com.yammer" %% "metrics" % "2.0.0-BETA10"
-  
+  val highScale = "com.boundary" % "high-scale-lib" % "1.0.1"
 }
