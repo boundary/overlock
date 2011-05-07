boundary/overlock

commit 5c79d195e512d3db80b9816918ca4421c4482a2b

cleanup the contract violations in getOrElseUpdate

src/main/scala/overlock/atomicmap/AtomicMap.scala
@@ -63,25 +63,30 @@ class AtomicMap[A,B](u : => JConcurrentMap[A,Any]) extends ConcurrentMap[A,B] {
   override def getOrElseUpdate(key : A, op : => B) : B = {
     val t = new OneShotThunk(op)
     under.putIfAbsent(key, t) match {
-      case null => 
-        val ve = t.value
-        if (under.replace(key, t, ve)) {
+      case null =>
+        try {
+          val ve = t.value
+          under.replace(key, t, ve)
+          //regardless of whether or not the replace worked, we must
+          //return ve to remain within the bounds of the getOrElseUpdate
+          // contract. If a concurrent update happened, this thread
+          // ought not to see it until after returning from this call.
           ve
-        } else { //will only happen if client code is mixing getOrElseUpdate and put
-          get(key) match {
-            case Some(thunk : OneShotThunk[_]) => thunk.value.asInstanceOf[B]
-            case Some(v : B) => v
-            case None => //loop around to the beginning
-              getOrElseUpdate(key, ve)
-          }
+        } catch {
+          case ex : Exception =>
+            under.remove(key, t)
+            throw ex
         }
-      case thunk : OneShotThunk[_] => thunk.value.asInstanceOf[B]
+      case OneShotThunk(v : B) => v
       case v : B => v
     }
   }
   
   def replace(key : A, value : B) : Option[B] = {
-    Option(under.replace(key,value)).map(_.asInstanceOf[B])
+    Option(under.replace(key,value)).map {
+      case b : B => b
+      case OneShotThunk(v : B) => v
+    }
   }
   
   /**
@@ -98,7 +103,10 @@ class AtomicMap[A,B](u : => JConcurrentMap[A,Any]) extends ConcurrentMap[A,B] {
   }
   
   def putIfAbsent(key : A, value : B) : Option[B] = {
-    Option(under.putIfAbsent(key,value)).map(_.asInstanceOf[B])
+    Option(under.putIfAbsent(key,value)).map {
+      case b : B => b
+      case OneShotThunk(v : B) => v
+    }
   }
   
   def -=(key : A) : this.type = {
@@ -115,7 +123,7 @@ class AtomicMap[A,B](u : => JConcurrentMap[A,Any]) extends ConcurrentMap[A,B] {
   def get(key : A) : Option[B] = {
     Option(under.get(key)) match {
       case None => None
-      case Some(t : OneShotThunk[_]) => Some(t.value.asInstanceOf[B])
+      case Some(OneShotThunk(v : B)) => Some(v)
       case Some(v : B) => Some(v)
     }
   }

src/main/scala/overlock/atomicmap/OneShotThunk.scala
@@ -19,10 +19,17 @@ package overlock.atomicmap
 import java.util.concurrent._
 import locks._
 
+object OneShotThunk {
+  def unapply[A](ost : OneShotThunk[_]) : Option[A] = {
+    ost.value match {
+      case v : A => Some(v)
+      case _ => None
+    }
+  }
+}
+
 class OneShotThunk[A](op : => A) {
-  //wonder what the scala compiler will cook up here
   lazy val value = op
-  
   
   override def equals(other : Any) = other match {
     case o : OneShotThunk[_] => value == o.value

commit dd8b05613a499b9500b433933a96f8c106b564fa

Resolve ClassCastException: "java.lang.String cannot be cast to scala.runtime.Nothing$" resulting when putIfAbsent returns a non-null value, triggering improper type inference of "Nothing" by explicitly matching on type OneShotThunk[B] and forcing evaluation of the thunk on return.
Issue appears to have been introduced by: https://github.com/boundary/overlock/commit/5c79d195e512d3db80b9816918ca4421c4482a2b#L0L78

[ Original output of "throughput" test triggering issue: https://gist.github.com/960867 ]

src/main/scala/overlock/atomicmap/AtomicMap.scala
@@ -77,7 +77,7 @@ class AtomicMap[A,B](u : => JConcurrentMap[A,Any]) extends ConcurrentMap[A,B] {
             under.remove(key, t)
             throw ex
         }
-      case OneShotThunk(v : B) => v
+      case thunk : OneShotThunk[B] => thunk.value
       case v : B => v
     }
   }