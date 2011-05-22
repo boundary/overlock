9af45b60d4ef3de312de9d524825895bb75ce039
cliff moon <cliff@leaninto.it>
2011-05-21 17:50:57 -0700

    bump the metrics version

--- project/build/OverlockProject.scala
+++ project/build/OverlockProject.scala
@@ -2,6 +2,7 @@ import sbt._
 import sbt.StringUtilities._
 
 class OverlockProject(info : ProjectInfo) extends DefaultProject(info) {
+  val coda = "Coda Hale's Repo" at "http://repo.codahale.com/"
   val boundary = "Boundary Public Repo" at "http://maven.boundary.com/artifactory/repo"
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Boundary Public Repo (Publish)" at "http://maven.boundary.com/artifactory/external"
@@ -9,7 +10,7 @@ class OverlockProject(info : ProjectInfo) extends DefaultProject(info) {
   Credentials(Path.userHome / ".ivy2" / ".credentials-external", log)
   
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test"
-  val metrics = "com.yammer" %% "metrics" % "2.0.0-BETA13-SNAPSHOT"
+  val metrics = "com.yammer.metrics" %% "metrics-core" % "2.0.0-BETA12"
   val highScale = "com.boundary" % "high-scale-lib" % "1.0.3"
   
   lazy val throughput = runTask(Some("overlock.atomicmap.ThroughputTest"), runClasspath).dependsOn(compile) describedAs "Runs the throughput test."
